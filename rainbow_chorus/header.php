4ee30238dd5b30308103e599b4954a88a3fe786c
Pascal Conges <pascal.conges@brewers.co.uk>
2016-04-05 20:05:25 +0100

    Attempt to fix css and img path

--- rainbow_chorus/header.php
+++ rainbow_chorus/header.php
@@ -8,8 +8,8 @@
     <script data-require="jquery@*" data-semver="2.2.0" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <link data-require="bootstrap-css@3.3.6" data-semver="3.3.6" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.css" />
     <link data-require="font-awesome@*" data-semver="4.5.0" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.css" />
-    <link rel="stylesheet" href="<?php echo get_template_directory(); ?>/utilities.css" />
-    <link rel="stylesheet" href="<?php echo get_template_directory(); ?>/style.css" />
+    <link rel="stylesheet" href="<?php get_template_directory_uri(); ?>/utilities.css" />
+    <!-- <link rel="stylesheet" href="<?php echo get_template_directory(); ?>/style.css" /> -->
     <script src="script.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
@@ -41,7 +41,7 @@
 	        <div class="rainbow_cache rainBlue"></div>
 
 	        <div class="container">
-	            <img src="img/logo.png" class="logo" alt="Rainbow Chorus Logo">
+	            <img src="<?php get_template_directory_uri(); ?>/img/logo.png" class="logo" alt="Rainbow Chorus Logo">
 	            <a class="menuIcon"><i class="fa fa-bars fa-2x"></i></a>
 	            <div class="bottom">
 	                <h1>Strength in Harmony</h1>
@@ -67,32 +67,32 @@
         <div class="row menu_balls">
             <div class="col-xs-4 col-sm-2">
                 <a href="/about-us">                
-                    <img src="img/menu/about.png" alt="" class="img-responsive">
+                    <img src="<?php get_template_directory_uri(); ?>/img/menu/about.png" alt="" class="img-responsive">
                 </a>
             </div>
             <div class="col-xs-4 col-sm-2">
                 <a href="/performances">                
-                    <img src="img/menu/join.png" alt="" class="img-responsive">
+                    <img src="<?php get_template_directory_uri(); ?>/img/menu/join.png" alt="" class="img-responsive">
                 </a>
             </div>
             <div class="col-xs-4 col-sm-2">
                 <a href="/join-us">                
-                    <img src="img/menu/perfs.png" alt="" class="img-responsive">
+                    <img src="<?php get_template_directory_uri(); ?>/img/menu/perfs.png" alt="" class="img-responsive">
                 </a>
             </div>
             <div class="col-xs-4 col-sm-2">
                 <a href="/press">                
-                    <img src="img/menu/press.png" alt="" class="img-responsive">
+                    <img src="<?php get_template_directory_uri(); ?>/img/menu/press.png" alt="" class="img-responsive">
                 </a>
             </div>
             <div class="col-xs-4 col-sm-2">
                 <a href="http://www.brownpapertickets.com/event/2386279">                
-                    <img src="img/menu/tickets.png" alt="" class="img-responsive">
+                    <img src="<?php get_template_directory_uri(); ?>/img/menu/tickets.png" alt="" class="img-responsive">
                 </a>
             </div>
             <div class="col-xs-4 col-sm-2">
                 <a href="/join-us/volunteer">                
-                    <img src="img/menu/volunteer.png" alt="" class="img-responsive">
+                    <img src="<?php get_template_directory_uri(); ?>/img/menu/volunteer.png" alt="" class="img-responsive">
                 </a>
             </div>
         </div>
